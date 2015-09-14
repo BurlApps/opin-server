e43a5c3ab545249afa9d304ce55a7e712a89d847
Brian Vallelunga <vallelungabrian@gmail.com>
2015-09-14 14:47:34 -0700

    updated code length

--- cloud/afterSave/class.js
+++ cloud/afterSave/class.js
@@ -2,7 +2,9 @@ Parse.Cloud.afterSave("Class", function(req, res) {
   var object = req.object
   
   if(object.existed()) return
+  
+  var code = object.id.toLowerCase().match(/[a-zA-Z]+/g).join("").slice(0,5)
 
-  object.set("code", object.id.toLowerCase())
+  object.set("code", code)
   object.save()
 })
